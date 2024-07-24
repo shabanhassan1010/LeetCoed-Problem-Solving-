7f2a1fdd2d0814904a15de318fd7047bf976e9f8
Shaban Hassan <127775327+shabanhassan1010@users.noreply.github.com>
2024-07-24 18:08:10 +0300

    [2215] [Time Beats: 23.57%] [Memory Beats: 11.17%] - LeetPush

--- 2215-Find-the-Difference-of-Two-Arrays.cs
+++ 2215-Find-the-Difference-of-Two-Arrays.cs
@@ -2,46 +2,13 @@ public class Solution
 {
     public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
     {
-        // Dictionaries to store counts of elements
-        Dictionary<int, bool> dict1 = new Dictionary<int, bool>();
-        Dictionary<int, bool> dict2 = new Dictionary<int, bool>();
+         // Convert arrays to HashSets to easily find differences
+        HashSet<int> set1 = new HashSet<int>(nums1);
+        HashSet<int> set2 = new HashSet<int>(nums2);
 
-        // Fill dictionaries with elements from nums1 and nums2
-        foreach (var num in nums1)
-        {
-            if (!dict1.ContainsKey(num))
-            {
-                dict1[num] = true;
-            }
-        }
-        
-        foreach (var num in nums2)
-        {
-            if (!dict2.ContainsKey(num))
-            {
-                dict2[num] = true;
-            }
-        }
-
-        // Find elements in nums1 not in nums2
-        IList<int> onlyInNums1 = new List<int>();
-        foreach (var num in dict1.Keys)
-        {
-            if (!dict2.ContainsKey(num))
-            {
-                onlyInNums1.Add(num);
-            }
-        }
-
-        // Find elements in nums2 not in nums1
-        IList<int> onlyInNums2 = new List<int>();
-        foreach (var num in dict2.Keys)
-        {
-            if (!dict1.ContainsKey(num))
-            {
-                onlyInNums2.Add(num);
-            }
-        }
+        // Find differences
+        IList<int> onlyInNums1 = set1.Except(set2).ToList();
+        IList<int> onlyInNums2 = set2.Except(set1).ToList();
 
         // Return results as a list of lists
         return new List<IList<int>> { onlyInNums1, onlyInNums2 };
