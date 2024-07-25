0427da1b66b64e6e0216d4fe583655bfdf830f30
Shaban Hassan <127775327+shabanhassan1010@users.noreply.github.com>
2024-07-25 23:47:50 +0300

    [2215] [Time Beats: 11.96%] [Memory Beats: 9.13%] - LeetPush

--- 2215-Find-the-Difference-of-Two-Arrays.cs
+++ 2215-Find-the-Difference-of-Two-Arrays.cs
@@ -2,11 +2,13 @@ public class Solution
 {
     public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
     {
+        // Input : nums1 = [1,2,3],   nums2 = [2,4,6]
+        // Output: [[1,3],[4,6]]
          // Convert arrays to HashSets to easily find differences
         HashSet<int> set1 = new HashSet<int>(nums1);
         HashSet<int> set2 = new HashSet<int>(nums2);
 
-        // Find differences
+        // Find differences and Retuen the value in list Not array or any type
         IList<int> onlyInNums1 = set1.Except(set2).ToList();
         IList<int> onlyInNums2 = set2.Except(set1).ToList();
 
