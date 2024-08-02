2d859eae9d10f3ce1a9b3661b6d961cbd1a097dd
Shaban Hassan <127775327+shabanhassan1010@users.noreply.github.com>
2024-08-02 03:49:50 +0300

    [121] [Time Beats: 38.79%] [Memory Beats: 84.45%] - LeetPush

--- 121-Best-Time-to-Buy-and-Sell-Stock.cs
+++ 121-Best-Time-to-Buy-and-Sell-Stock.cs
@@ -2,8 +2,8 @@ public class Solution
 {
     public int MaxProfit(int[] prices) 
     {
-        int maxProfit = 0 ;
-        int minPrice = prices[0];
+        int maxProfit = 0 ; // Initialize maxProfit to 0, as initially, no transactions have been made
+        int minPrice = prices[0];  // Set minPrice to the price of the stock on the first day.
         for (int i = 1; i < prices.Length; i++)
         {
             if (prices[i] < minPrice)
