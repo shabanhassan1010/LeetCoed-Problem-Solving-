39961f8588127a1bd024a6bbc276b2eb2b17dfdc
Shaban Hassan <127775327+shabanhassan1010@users.noreply.github.com>
2024-08-21 04:00:59 +0300

    [3151] [Time Beats: 8.82%] [Memory Beats: 55.15%] - LeetPush

--- 3151-Special-Array-I.cs
+++ 3151-Special-Array-I.cs
@@ -2,9 +2,9 @@ public class Solution {
     public bool IsArraySpecial(int[] nums) 
     {
         bool answer = true;
-        for(int i = 0; i < nums.Length - 1; i++) // Use nums.Length
+        for(int i = 0; i < nums.Length - 1; i++) // Iterate from the first element to the second-to-last element
         {
-            if(nums[i] % 2 == nums[i + 1] % 2) 
+            if(nums[i] % 2 == nums[i + 1] % 2)  // Check if current and next elements have the same parity
             {
                 answer = false;
                 break; // The break should be inside the if block
