8505350b11278180aeb925caf3e7ee33ad2b8dde
Shaban Hassan <127775327+shabanhassan1010@users.noreply.github.com>
2024-09-26 02:45:36 +0300

    [1929] [Time Beats: 85.23%] [Memory Beats: 94.47%] - LeetPush

--- 1929-Concatenation-of-Array.cs
+++ 1929-Concatenation-of-Array.cs
@@ -1,18 +1,17 @@
 public class Solution
  {
     public int[] GetConcatenation(int[] nums) 
-    {
-        
-        int n = nums.Length;   // lw El-Length = 5
-        int[] res = new int[n * 2]; // hna ana 3amlt array  El-Length = 10
-        for (int i = 0; i < n; i++)
+    {       
+        //int n = nums.Length;   // lw El-Length = 5
+        int[] arr2 = new int[nums.Length * 2]; // hna ana 3amlt array  El-Length = 10
+        for (int i = 0; i < nums.Length; i++)
         {
-            res[i] = nums[i]; // set all index from nums array in res[i], now res[i] feha array nums
+         arr2[i] = nums[i]; // set all index from nums array in arr2[i],now arr2[i] feha array nums
         }
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < nums.Length; i++)
         {
-            res[ n + i ] = nums[i]; // hna ana add copy of El-Array 
+            arr2[ nums.Length + i ] = nums[i]; // hna ana add copy of El-Array 
         }
-        return res;
+        return arr2;
     }
 }
