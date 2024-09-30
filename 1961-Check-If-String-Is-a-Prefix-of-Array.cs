98d31ae283faec5a986720fe242962379f0b23f0
Shaban Hassan <127775327+shabanhassan1010@users.noreply.github.com>
2024-10-01 02:55:05 +0300

    [1961] [Time Beats: 94.44%] [Memory Beats: 23.15%] - LeetPush

--- 1961-Check-If-String-Is-a-Prefix-of-Array.cs
+++ 1961-Check-If-String-Is-a-Prefix-of-Array.cs
@@ -1,20 +1,13 @@
 public class Solution {
-    public bool IsPrefixString(string s, string[] words) {
-        string prefix = "";
-    
+    public bool IsPrefixString(string s, string[] words) 
+    {
+    string prefix = "";
     foreach (string word in words)
     {
-        prefix += word;
-        
-        // If the current prefix matches 's', return true
-        if (prefix == s)
-            return true;
-        
-        // If the prefix becomes longer than 's', return false
-        if (prefix.Length > s.Length)
-            return false;
+        prefix += word;       
+        if(prefix == s) return true;
+       
     }
-    
-    return false; // Return false if no match is found
+    return false;   // Return false if no match is found
     }
 }
